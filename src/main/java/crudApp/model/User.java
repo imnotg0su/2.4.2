49c72764ee354f8944dade66a0d9ba3a41bac86b
Pavel Gubaev <imhere88800@gmail.com>
2021-12-13 16:49:11 +0800

    small changed

--- src/main/java/crudApp/model/User.java
+++ src/main/java/crudApp/model/User.java
@@ -27,8 +27,7 @@ public class User {
     public User() {
     }
 
-    public User(int id, String name, String surname, int age, String status) {
-        this.id = id;
+    public User(String name, String surname, int age, String status) {
         this.name = name;
         this.surname = surname;
         this.age = age;
